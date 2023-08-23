65dc5c4c7743877bdfca06ac8d527b76ffa794fb
Eric Escobar <eryc@live.com.ar>
2023-08-23 20:43:33 -0300

    upd

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -3,7 +3,6 @@ import { getAuth } from 'firebase/auth';
 import { getStorage, uploadBytes, ref, getDownloadURL } from 'firebase/storage';
 import { v4 } from 'uuid'
 
-/*
 const firebaseConfig = {
   apiKey: "AIzaSyAsMo-3sbQKGtOZjLisPh49Fuakoj29lKc",
   authDomain: "buenospintores-23a2b.firebaseapp.com",
@@ -13,7 +12,7 @@ const firebaseConfig = {
   appId: "1:411230213668:web:0ed438548a195504402ab1",
   measurementId: "G-2LGHJFLZBM"
 };
-*/
+/*
 
 const firebaseConfig = {
   apiKey: "AIzaSyBAUuQ0CI_6KxqPtgTFDGdjo7VMhg0uTdU",
@@ -23,6 +22,8 @@ const firebaseConfig = {
   messagingSenderId: "444273763519",
   appId: "1:444273763519:web:1f5ff3b652d5be8a4e520a"
 };
+*/
+
 
 const app = initializeApp(firebaseConfig);
 export const firebaseAuth = getAuth(app);
